7d5571df10e3d592bb2d0f34e171dc330ae47f49
ambarnett <andy.barnett09@gmail.com>
2021-08-24 13:53:27 -0500

    inital commit

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+Console.WriteLine("Try to guess the secret number!");
+Console.Write("Enter a number: ");
+string input = Console.ReadLine();
+int parsedInput = int.Parse(input);
+
+int secNum = 42;
